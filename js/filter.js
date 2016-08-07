07b4121a25fe0d514b57da6ea2b69a0159002663
Tom Lorimer <teachable@showme.ninja>
2016-08-07 21:04:26 +0100

    Created the javascript file and added the requirements for the project. Added the script tag to the index.html to reference to the new JS file.

---
+++ js/filter.js
@@ -0,0 +1,32 @@
+/**
+ * Created by tlorimer on 07/08/2016.
+ */
+
+// show only 10 students at a time
+
+// caluculate the total number of pages to show at the bottom of the page
+
+// Hide all but the first 10 students
+
+// When the user clicks on "2" in the pagination, students 11 through 20 are shown.
+    //This should work no matter the size of the list of students or the number of pages
+
+// Add search markup using progressive enhancement (use filters-example.html for markup)
+
+// Add event listener to the search button.
+    // When the user click on the button it should use the text in the search input to filter the results
+    // Searching should be case insensitive
+
+// Users should be able to search by name or email address.
+    // Partial matching should be displayed in results
+
+// Search results should also be paginated
+
+// EXTRA CREDIT //
+//////////////////
+
+// Include simple animation when transitioning between pages
+
+// As the user types in the search box, dynamicaly filter the student listing
+
+// If no matches are found, include a message in the HTML to tell the user there are no matches
