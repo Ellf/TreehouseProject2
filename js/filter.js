174ae460e22aaba65be1816c4f2ce1b27f77b49a
ellf <tom@lorimer.email>
2016-09-30 22:35:56 +0100

    Got the correct number of students to appear and added basic pagination framework. Saving to make sure I don't forget.

--- js/filter.js
+++ js/filter.js
@@ -14,20 +14,49 @@ console.log("numberOfPages: ", numberOfPages);
 
 // Hide all students as a start
 for (var x = 0; x < numberOfStudents; x+=1 ) {
-    studentItem[x].style.visibility = "hidden";
+    studentItem[x].style.display = "none";
 }
 
 // loop through the number of pages
 var pageStart = currentPage * numberOfPages;
 var pageEnd = pageStart + 10;
-for (var pages = pageStart; x <= pageEnd; pages += 1) {
-    console.log("pageStart: ", pageStart);
-    console.log("pageEnd: ", pageEnd);
-    studentItem[pages].style.visibilty = "visible";
+for (var pages = pageStart; pages < pageEnd; pages += 1) {
+    console.log("pages: ", pages);
+    console.log("length: ", studentItem.length);
+    studentItem[pages].style.display = "inherit";
 }
+
+// Add pagination
+// Target the class 'page'
+var el = document.getElementsByClassName('page')[0];
+    // Add a new div element
+    elChild = document.createElement("div");
+    // Create the li nagivation framework with the 'pagination' class
+     var pageFramework = "<ul class='pagination'>";
+
+    // apply the page numbers and id elements to the list items
+    for (var x = 1; x <= numberOfPages; x += 1) {
+        pageFramework += "<li>";
+        // make the first page active
+        if (x == 1) {
+            pageFramework += "<a class='active' id=" + x + " href='#'>" + x + "</a>";
+        } else {
+            pageFramework += "<a id='pg-" + x + "' href='#'>" + x + "</a>";
+        }
+        pageFramework += "</li>";
+    }
+    pageFramework += "</ul>";
+
+    elChild.innerHTML = pageFramework;
+
+// append (insert at the end) the pagination div to the page class element
+el.appendChild(elChild);
 
 // When the user clicks on "2" in the pagination, students 11 through 20 are shown.
     //This should work no matter the size of the list of students or the number of pages
+var picked = document.getElementById()
+
+
 
 // Add search markup using progressive enhancement (use filters-example.html for markup)
 
